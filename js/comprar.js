0b1d09c308ebee27c8c5dd88fbe35037a5579ccb
Flor5307 <104154103+Flor5307@users.noreply.github.com>
2022-07-27 20:22:58 -0300

    carrito terminado

--- js/comprar.js
+++ js/comprar.js
@@ -1,90 +1,3 @@
-document.getElementById('submit_panes').onclick = function() {
-    let select_tipo_pan = document.getElementById('Panes01');
-    let tipo_pan = select_tipo_pan.options[select_tipo_pan.selectedIndex].text;
-
-    let select_cantidad_pan = document.getElementById('Panes02');
-    let cantidad_pan = select_cantidad_pan.options[select_cantidad_pan.selectedIndex].text;
-
-
-    switch (tipo_pan) {
-        case "De centeno":
-            alert( 700 * cantidad_pan);
-            break;
-        case "Blanco":
-            alert( 400 * cantidad_pan);
-            break;
-        case "Relleno":
-            alert( 650 * cantidad_pan);
-            break;
-        case "Multicereal":
-            alert( 550 * cantidad_pan);
-            break;
-        case "Blanco con nueces":
-            alert(850 * cantidad_pan);
-            break;
-        default:
-            alert(0);
-            break;
-    } 
-} 
-
-document.getElementById('submit_pastas').onclick = function() {
-    let select_tipo_pastas = document.getElementById('Pastas01');
-    let tipo_pastas = select_tipo_pastas.options[select_tipo_pastas.selectedIndex].text;
-
-    let select_cantidad_pastas = document.getElementById('Pastas02');
-    let cantidad_pastas = select_cantidad_pastas.options[select_cantidad_pastas.selectedIndex].text;
-
-    switch (tipo_pastas) {
-        case "Ñoquis rellenos":
-            alert( 580 * cantidad_pastas);
-            break;
-        case "Ñoquis":
-            alert( 450 * cantidad_pastas);
-            break;
-        case "Fideos verdes, violetas o al huevo":
-            alert( 350 * cantidad_pastas);
-            break;
-        case "Capellettis comunes":
-            alert( 500 * cantidad_pastas);
-            break;
-        case "Capellettis especiales":
-            alert(550 * cantidad_pastas);
-            break;
-        default:
-            alert(0);
-            break;
-    } 
-} 
-document.getElementById('submit_salsas').onclick = function() {
-    let select_tipo_salsas = document.getElementById('Salsas01');
-    let tipo_salsas = select_tipo_salsas.options[select_tipo_salsas.selectedIndex].text;
-
-    let select_cantidad_salsas = document.getElementById('Salsas02');
-    let cantidad_salsas = select_cantidad_salsas.options[select_cantidad_salsas.selectedIndex].text;
-
-    switch (tipo_salsas) {
-        case "Bolognesa":
-            alert( 550 * cantidad_salsas);
-            break;
-        case "Crema de champignones":
-            alert( 450 * cantidad_salsas);
-            break;
-        case "Crema de limon":
-            alert( 340 * cantidad_salsas);
-            break;
-        case "Napolitana":
-            alert( 300 * cantidad_salsas);
-            break;
-        case "Tomate, aceituna y ajo":
-            alert(450 * cantidad_salsas);
-            break;
-        default:
-            alert(0);
-            break;
-    } 
-} 
-
 //USUARIO DE TARJETA
 
 let tarjeta_usuario = []
@@ -109,48 +22,118 @@ buton.addEventListener("click", save_data);
 
 //¿Que tipo de comensal sos?
 
+let pasta_1 = document.getElementById("pasta_1");
+let pasta_2 = document.getElementById("pasta_2");
+let pasta_3 = document.getElementById("pasta_3");
+let pasta_4 = document.getElementById("pasta_4");
+let pasta_5 = document.getElementById("pasta_5");
+
+let pan_1 = document.getElementById("pan_1");
+let pan_2 = document.getElementById("pan_2");
+let pan_3 = document.getElementById("pan_3");
+let pan_4 = document.getElementById("pan_4");
+let pan_5 = document.getElementById("pan_5");
+
+let salsa_1 = document.getElementById("salsa_1");
+let salsa_2 = document.getElementById("salsa_2");
+let salsa_3 = document.getElementById("salsa_3");
+let salsa_4 = document.getElementById("salsa_4");
+let salsa_5 = document.getElementById("salsa_5");
+
 let comensal_1 = document.getElementById("comensal_1");
 
-comensal_1.addEventListener("click", function(){
-    console.log("Productos para veganos", vegano)
-    let ñoquisRellenos = document.getElementById("ñoquisRellenos"); ñoquisRellenos.remove();
-    let capelletis = document.getElementById("capelletis"); capelletis.remove();
-    let capelletisEspeciales = document.getElementById("capelletisEspeciales"); capelletisEspeciales.remove();
-    let bolognesa = document.getElementById("bolognesa"); bolognesa.remove();
-    let champi = document.getElementById("champi"); champi.remove();
-    let limon = document.getElementById("limon"); limon.remove();
+comensal_1.addEventListener("click", function () {
+    console.log("Productos aptos para todos/as", como_de_todo)
+
+    pasta_1.style.display = "block";
+    pasta_2.style.display = "block";
+    pasta_3.style.display = "block";
+    pasta_4.style.display = "block";
+    pasta_5.style.display = "block";
+
+    pan_1.style.display = "block";
+    pan_2.style.display = "block";
+    pan_3.style.display = "block";
+    pan_4.style.display = "block";
+    pan_5.style.display = "block";
+
+    salsa_1.style.display = "block";
+    salsa_2.style.display = "block";
+    salsa_3.style.display = "block";
+    salsa_4.style.display = "block";
+    salsa_5.style.display = "block";
 });
 
 let comensal_2 = document.getElementById("comensal_2");
 
-comensal_2.addEventListener("click", function(){
-    console.log("Productos para vegetarianos", vegetariano)
-    let capelletisEspeciales = document.getElementById("capelletisEspeciales"); capelletisEspeciales.remove();
-    let bolognesa = document.getElementById("bolognesa"); bolognesa.remove();
+comensal_2.addEventListener("click", function () {
+    console.log("Productos para veganos", vegano)
+
+    pasta_1.style.display = "block";
+    pasta_2.style.display = "none";
+    pasta_3.style.display = "block";
+    pasta_4.style.display = "none";
+    pasta_5.style.display = "none";
+
+    pan_1.style.display = "block";
+    pan_2.style.display = "block";
+    pan_3.style.display = "block";
+    pan_4.style.display = "block";
+    pan_5.style.display = "block";
+
+    salsa_1.style.display = "none";
+    salsa_2.style.display = "none";
+    salsa_3.style.display = "none";
+    salsa_4.style.display = "block";
+    salsa_5.style.display = "block";
 });
 
 let comensal_3 = document.getElementById("comensal_3");
 
-comensal_3.addEventListener("click", function(){
-    let ñoquisRellenos = document.getElementById("ñoquisRellenos"); ñoquisRellenos.remove();
-    let capelletis = document.getElementById("capelletis"); capelletis.remove();
-    let capelletisEspeciales = document.getElementById("capelletisEspeciales"); capelletisEspeciales.remove();
-    let centeno = document.getElementById("centeno"); centeno.remove();
-    let blanco = document.getElementById("blanco"); blanco.remove();
-    let relleno = document.getElementById("relleno"); relleno.remove();
-    let multicereal = document.getElementById("multicereal"); multicereal.remove();
-    let conNueces = document.getElementById("conNueces"); conNueces.remove();
-    let ñoquis = document.getElementById("ñoquis"); ñoquis.remove();
-    let fideos = document.getElementById("fideos"); fideos.remove();
-    alert("Perdon, solo tenemos salsas sin TACC")
+comensal_3.addEventListener("click", function () {
+    console.log("Productos para vegetarianos", vegetariano)
+
+    pasta_1.style.display = "block";
+    pasta_2.style.display = "block";
+    pasta_3.style.display = "block";
+    pasta_4.style.display = "block";
+    pasta_5.style.display = "none";
+
+    pan_1.style.display = "block";
+    pan_2.style.display = "block";
+    pan_3.style.display = "block";
+    pan_4.style.display = "block";
+    pan_5.style.display = "block";
+
+    salsa_1.style.display = "none";
+    salsa_2.style.display = "block";
+    salsa_3.style.display = "block";
+    salsa_4.style.display = "block";
+    salsa_5.style.display = "block";
 });
 
 let comensal_4 = document.getElementById("comensal_4");
 
-comensal_4.addEventListener("click", function(){
-    console.log("Productos aptos para todos/as", como_de_todo)
-});
+comensal_4.addEventListener("click", function () {
 
+    pasta_1.style.display = "none";
+    pasta_2.style.display = "none";
+    pasta_3.style.display = "none";
+    pasta_4.style.display = "none";
+    pasta_5.style.display = "none";
+
+    pan_1.style.display = "none";
+    pan_2.style.display = "none";
+    pan_3.style.display = "none";
+    pan_4.style.display = "none";
+    pan_5.style.display = "none";
+
+    salsa_1.style.display = "block";
+    salsa_2.style.display = "block";
+    salsa_3.style.display = "block";
+    salsa_4.style.display = "block";
+    salsa_5.style.display = "block";
+}); 
 
 let vegano = [
     {
@@ -342,4 +325,4 @@ let como_de_todo = [
         categoria: "Panes",
         precio: "850",
     },
-];
+]; 
