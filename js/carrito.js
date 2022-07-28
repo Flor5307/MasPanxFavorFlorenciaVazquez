5cac849e3c49d506c8235afb082b3798fc68c658
Flor5307 <104154103+Flor5307@users.noreply.github.com>
2022-07-28 18:59:56 -0300

    carrito terminado

--- js/carrito.js
+++ js/carrito.js
@@ -1,64 +1,53 @@
 let btn_compra = document.querySelectorAll(".botonCompra");
 console.log(btn_compra);
 
-for( let boton of btn_compra){
-    boton.addEventListener("click" , agregar_a_carrito);
+for (let boton of btn_compra) {
+  boton.addEventListener("click", agregar_a_carrito);
 }
+
 let carrito = [];
-function agregar_a_carrito(e){
-    console.log("EL EVENTO ESTA EN:" ,e.target);
-    let hijo = e.target;
-    let padre = hijo.parentNode;
-    let abuelo = padre.parentNode;
-    let nombre_producto = padre.querySelector("h4").textContent;
-    let precio = padre.querySelector("span").textContent;
-    let producto = {
-        nombre: nombre_producto,
-        precio: precio,
-        cantidad: 1
-    };
-    carrito.push(producto);
-    let arreglo_JSON = JSON.stringify(carrito);
-    localStorage.setItem("carrito" , arreglo_JSON);
 
-    console.log(carrito);
+function agregar_a_carrito(e) {
+  //console.log("EL EVENTO ESTA EN:", e.target);
+  let hijo = e.target;
+  let padre = hijo.parentNode;
+  let abuelo = padre.parentNode;
 
-    mostrar_carrito( producto);
+  let nombre_producto = padre.querySelector("h4").textContent;
+  let precio = padre.querySelector("span").textContent;
+
+  let producto = {
+    nombre: nombre_producto,
+    precio: precio,
+    cantidad: 1,
+  };
+
+  carrito.push(producto);
+  let arreglo_JSON = JSON.stringify(carrito);
+  localStorage.setItem("carrito", arreglo_JSON);
+  console.log(carrito);
+
+  mostrar_carrito(producto);
 }
 
-
-
-
-function mostrar_carrito( producto){
-
-
-    let fila = document.createElement("tr");
-
-    fila.innerHTML = `<td>${producto.nombre}</td>
+function mostrar_carrito(producto) {
+  let fila = document.createElement("tr");
+  fila.innerHTML = `<td>${producto.nombre}</td>
                       <td>${producto.cantidad}</td>
                       <td>${producto.precio}</td>
-                      <td><button class="btn-danger borrar_elemento">Borrar</button></td>`;
+                      <td><button class="btn-dark borrar_elemento">Borrar</button></td>`;
 
-    
-    let tabla = document.getElementById("tbody");
+  let tabla = document.getElementById("tbody");
+  tabla.append(fila);
 
-    tabla.append(fila);
+  let botones_borrar = document.querySelectorAll(".borrar_elemento");
 
-
-    let botones_borrar = document.querySelectorAll(".borrar_elemento");
-
-    for( let boton of botones_borrar){
-
-        boton.addEventListener("click" , borrar_producto);
-    }
-
-
+  for (let boton of botones_borrar) {
+    boton.addEventListener("click", borrar_producto);
+  }
 }
 
-
-function borrar_producto(e){
-
-    let abuelo = e.target.parentNode.parentNode;
-    abuelo.remove();
-    
-} 
+function borrar_producto(e) {
+  let abuelo = e.target.parentNode.parentNode;
+  abuelo.remove();
+}
